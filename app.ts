446d75f7247530db2486a6483b7d19070ab86e41
el7alafawy <66574111+el7alafawy@users.noreply.github.com>
2024-07-22 01:26:34 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -24,7 +24,7 @@ app.use(cookieParser());
 // cors
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000','https://al-doctor.vercel.app'],
     credentials:true,
   })
 );
